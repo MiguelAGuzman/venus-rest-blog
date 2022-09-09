a84e34db24a7b3bd095787d9a7e14bb7fe711949
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-09-08 20:06:36 -0500

    rest blog cont

---
+++ src/main/java/miguelguzman/venusrestblog/DogFactController.java
@@ -0,0 +1,22 @@
+package miguelguzman.venusrestblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+public class DogFactController {
+    @GetMapping
+    @ResponseBody
+    public String fetchDogFact() {
+        return "Puppies are a pain in the butt!";
+    }
+
+    @GetMapping("/dogo/{id}")
+    @ResponseBody
+    public String fetchSingleDogFact (@PathVariable long id){
+//fetch the particular dog fact w/matching id from database
+        return FakeDogFactGateway.fetchDogFactById(id);
+    }
+}
